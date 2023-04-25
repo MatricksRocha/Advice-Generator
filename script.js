f13914d142b507821f57ff2d831563d2f2145d80
Matricks Rocha <matricksrocha11@gmail.com>
2023-04-25 17:03:03 -0300

    fixing the usage of the async/await, using try/catch instead of then/catch

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // using the [Advice Slip API](https://api.adviceslip.com) to generate the advice
 const generateAdvice = async () => {
+    const adviceID = document.querySelector('#adviceID');
+    const adviceText = document.querySelector('#adviceText');
+    const loadingScreen = document.querySelector('#loadingScreen');
     const url = "https://api.adviceslip.com/advice";
 
-    await axios.get(url)
-        .then(response => {
-            const data = response.data.slip;
-            console.log(data)
-            adviceID.textContent = `ADVICE #${data.id}`;
-            adviceText.textContent = `"${data.advice}"`;
-        })
-        .catch(error => console.error(error))
+    loadingScreen.style.display = "initial";
+    try {
+        const response = await axios.get(url);
+        const data = response.data.slip;
+        console.log(data);
+        adviceID.textContent = `ADVICE #${data.id}`;
+        adviceText.textContent = `"${data.advice}"`;
+    } catch (error) {
+        console.error(error);
+    }
+    loadingScreen.style.display = "none";
 }
 
 diceButton.addEventListener('click', () => {generateAdvice()});
